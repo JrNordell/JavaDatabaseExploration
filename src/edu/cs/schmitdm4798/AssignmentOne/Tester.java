454892abeba2389e0397cbdf11fa6d341ddd9af1
DaveMSchmitz <g1phonedave@gmail.com>
2016-10-16 19:52:58 -0500

    Update tester to create a conflict
    experimenting with how to handle conflicts

--- src/edu/cs/schmitdm4798/AssignmentOne/Tester.java
+++ src/edu/cs/schmitdm4798/AssignmentOne/Tester.java
@@ -6,7 +6,7 @@ package edu.cs.schmitdm4798.AssignmentOne;
 public class Tester {
 
     public static void main(String[] args) {
-        Author author = new Author("Dave", "Schmitz");
+        Author author = new Author("Jack", "Schmitz");
         Book book1 = new Book("111", "Dave's edu.cs.schmitdm4798.AssignmentOne.Book ONE", 29.99F, "Barnes", 5);
         Book book1location = new Book("111", "Dave's edu.cs.schmitdm4798.AssignmentOne.Book ONE", 29.99F, "Apples", 4);
         Book book2 = new Book("112", "Dave's edu.cs.schmitdm4798.AssignmentOne.Book TWO", 29.99F, "Barnes", 3);
